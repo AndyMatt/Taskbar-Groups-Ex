a01bd423a228e3c5a0a2825e45ad8cc9c553dc29
Andrew <andy.matt89@gmail.com>
2025-03-07 16:37:54 +1100

    Updated Assembly information

--- TaskbarGroupsEx/App.cs
+++ TaskbarGroupsEx/App.cs
@@ -4,6 +4,9 @@ using System.Runtime;
 using System.Windows;
 using TaskbarGroupsEx.Classes;
 using TaskbarGroupsEx.Forms;
+using System.Reflection;
+
+[assembly: AssemblyKeyFileAttribute("TaskbarGroupsEx.snk")]
 
 namespace TaskbarGroupsEx
 {
