83e467850cbb1a5e618da703bf341216f93e48f8
Andrew <andy.matt89@gmail.com>
2025-04-07 20:16:00 +1100

    Added Basic FileHandler Helper Class
    FileHandler takes a filepath string and returns a MemoryStream

---
+++ TaskbarGroupsEx/Handlers/FileHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskbarGroupsEx.Handlers
+{
+    static class FileHandler
+    {
+        public static MemoryStream? GetMemoryStream(string filePath)
+        {
+            try
+            {
+                using (FileStream fs = File.Open(filePath, FileMode.Open, FileAccess.Read))
+                {
+                    MemoryStream memoryStream = new MemoryStream();
+                    fs.CopyTo(memoryStream);
+                    fs.Close();
+                    return memoryStream;
+                }
+            }
+            catch { }
+            return null;
+        }
+    }
+}
